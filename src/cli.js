b27fa4bfee934463b2ba107285f6d0228fd2c719
Nick Shaydayuk <ghost2394228@gmail.com>
2023-07-14 16:59:42 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,5 @@ import readlineSync from "readline-sync";
 export const greeting = () => {
   console.log("Welcome to the Brain Games!");
   var userName = readlineSync.question("May I have your name? ");
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 }
